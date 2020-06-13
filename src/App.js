e51549b8242b6c1f76be8184305722da0f23e441
narteysarso <narteysarso@gmail.com>
2020-06-13 11:30:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
+import Container from './components/Container';
+import Row from './components/Row';
+import Col from './components/Col';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              AC
+          </Button>
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              +/-
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              %
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.goldButton} block >
+              /
+          </Button>          
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              7
+          </Button>
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              8
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              9
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.goldButton} block >
+              X
+          </Button>          
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              4
+          </Button>
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              5
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              6
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.goldButton} block >
+              -
+          </Button>          
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              1
+          </Button>
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              2
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              3
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.goldButton} block >
+              +
+          </Button>          
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={6}>
+          <Button style={Styles.greyButton} block >
+              0
+          </Button>
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.greyButton} block >
+              .
+          </Button>         
+        </Col>
+        <Col size={3}>
+          <Button style={Styles.goldButton} block >
+              =
+          </Button>          
+        </Col>
+      </Row>
+      
+      
+      
+      
+    </Container>
   );
+}
+
+const Styles = {
+  goldButton: { 
+    backgroundColor: "gold",
+    borderColor: "#333", 
+    borderWidth: 0.5, 
+    borderRadius: 0,
+   color: "#fff" },
+  greyButton: { 
+    backgroundColor: "#ddd",
+    borderColor: "#333", 
+    borderWidth: 0.5, 
+    borderRadius: 0,
+    color: "#333" }
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = (props) => {
+    
+    const {style, title, clickHandler, block=false} = props;
+
+    return (
+        <button style={style} className={`btn ${block ? 'btn-block': ''} `} onClick={clickHandler}>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button;
